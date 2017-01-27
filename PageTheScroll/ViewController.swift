c7b3f4a63c91a9bc4e941ef58b2ebee3254afd5c
Jalal Bajrai <j.bajrai@gmail.com>
2017-01-27 08:27:40 +0300

    Add images to Aray by the for loop

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //let us to make aray for images we need to add contanet to ScrollView
+    var images = [UIImageView]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image: image))
+        }
+        
+        print("Count: \(images.count)")
     }
 
     override func didReceiveMemoryWarning() {
