951afcf6ddfe9ce98d7f0e1f74bcf2b38f4d3b86
Jalal Bajrai <j.bajrai@gmail.com>
2017-01-29 19:54:09 +0300

    finsh ScrollView

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,26 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     //let us to make aray for images we need to add contanet to ScrollView
     var images = [UIImageView]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        
+        print("Scrollview width: \(scrollView.frame.size.width)")
+        
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            // set frame in image view
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            
         }
         
-        print("Count: \(images.count)")
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
